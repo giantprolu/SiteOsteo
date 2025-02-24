11ab1afa45d44f37bf7aad6d1ea6e989e44900f4
giantprolu <nathnathchav@gamil.com>
2025-02-24 14:49:56 +0100

    test scrore phone

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SpeedInsights />
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
